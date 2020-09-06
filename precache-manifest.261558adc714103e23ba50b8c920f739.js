6a706dc6748962b4ba9336f850d3882ad68ea3d9
Elena Kashtanova <lenachestnut@gmail.com>
2020-09-06 19:56:53 +0300

    Updates

---
+++ precache-manifest.261558adc714103e23ba50b8c920f739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819c1bddba4a613812f2345fe33931c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3426677491e0d8c4ad0",
+    "url": "./static/css/main.7a16a3f4.chunk.css"
+  },
+  {
+    "revision": "b38200f718d493057e38",
+    "url": "./static/js/2.892c5d3d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.892c5d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3426677491e0d8c4ad0",
+    "url": "./static/js/main.51666f47.chunk.js"
+  },
+  {
+    "revision": "5acfd12ba8d6c9ddfbe3",
+    "url": "./static/js/runtime-main.94c40845.js"
+  }
+]);
